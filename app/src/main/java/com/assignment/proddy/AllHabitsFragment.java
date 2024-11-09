62810df0bee8a975e20cfba1e46a6e6ac3cc8942
Tahira-Fatimah <tahirafatimah27@gmail.com>
2024-11-10 01:14:52 +0500

    added habit list fragment layout

--- app/src/main/java/com/assignment/proddy/AllHabitsFragment.java
+++ app/src/main/java/com/assignment/proddy/AllHabitsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Tab1Fragment#newInstance} factory method to
+ * Use the {@link AllHabitsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Tab1Fragment extends Fragment {
+public class AllHabitsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Tab1Fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Tab1Fragment() {
+    public AllHabitsFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class Tab1Fragment extends Fragment {
      * @return A new instance of fragment Tab1Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Tab1Fragment newInstance(String param1, String param2) {
-        Tab1Fragment fragment = new Tab1Fragment();
+    public static AllHabitsFragment newInstance(String param1, String param2) {
+        AllHabitsFragment fragment = new AllHabitsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Tab1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        return inflater.inflate(R.layout.fragment_habits, container, false);
     }
 }
